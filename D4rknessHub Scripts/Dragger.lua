3476704d2c0ebff7ea5579f92204cc3da387cc6d
4ladydansmi31 <dansmi31primaryemail@gmail.com>
2026-05-28 16:42:48 +0300

    Update Dragger.lua

--- D4rknessHub Scripts/Dragger.lua
+++ D4rknessHub Scripts/Dragger.lua
@@ -1,6 +1,7 @@
 local v = {}
+local connections = {}
 function v:drag(gui, CanDrag)
-  local UserInputService = game:GetService("UserInputService")
+	local UserInputService = game:GetService("UserInputService")
 	local dragging
 	local dragInput
 	local dragStart
@@ -11,7 +12,7 @@ function v:drag(gui, CanDrag)
 		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
 	end
 
-	gui.InputBegan:Connect(function(input)
+	local conn1 = gui.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 and CanDrag or input.UserInputType == Enum.UserInputType.Touch and CanDrag then
 			dragging = true
 			dragStart = input.Position
@@ -25,16 +26,27 @@ function v:drag(gui, CanDrag)
 		end
 	end)
 
-	gui.InputChanged:Connect(function(input)
+	local conn2 = gui.InputChanged:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseMovement and CanDrag or input.UserInputType == Enum.UserInputType.Touch and CanDrag then
 			dragInput = input
 		end
 	end)
 
-	UserInputService.InputChanged:Connect(function(input)
+	local conn3 = UserInputService.InputChanged:Connect(function(input)
 		if input == dragInput and dragging and CanDrag then
 			update(input)
 		end
 	end)
+	
+	table.insert(connections, conn1)
+	table.insert(connections, conn2)
+	table.insert(connections, conn3)
 end
+
+function v:undrag(gui)
+	for i = 1, #connections do
+		connections[i]:Disconnect()
+	end
+end
+
 return v
